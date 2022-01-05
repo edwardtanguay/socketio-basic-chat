4d498a580b0fadd999f7892d69c2d7f53abfb145
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-05 17:12:46 +0100

    refactored import

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
+import http from 'http';
+import path from 'path';
 import express from 'express';
 import { Server as socketIO } from 'socket.io';
 
 const app = express();
 const PORT = 5011;
+const __dirname = path.resolve(path.dirname(''));
+const server = http.Server(app);
+const io = new socketIO(server);
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/public/index.html');
 });
 
+io.on('connection', (socket) => {
+	console.log('user connected');
+	socket.emit('greeting-from-server', {
+		greeting: 'you loaded the page'
+	});
+	socket.broadcast.emit('server-broadcast-message', {
+		greeting: 'NEW CLIENT CONNNECTED '
+	});
+})
 
 app.listen(PORT, () => {
 	console.log(`listening on port http://localhost:${PORT}`);
